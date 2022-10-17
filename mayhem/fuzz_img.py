78ac4a42928362b8db684cf3447a820db5d1bafa
ennamarie19 <dallmeyerm@att.net>
2022-10-17 16:21:57 -0500

    Fixed typos

--- mayhem/fuzz_img.py
+++ mayhem/fuzz_img.py
@@ -1,25 +1,19 @@
 #! /usr/bin/python3
-import xml.etree.ElementTree
 
 import atheris
 import sys
-import io
 import tempfile
 
 with atheris.instrument_imports():
     import imagesize
 
-supported_ext = [".png"]
-
 def TestOneInput(data):
         fdp = atheris.FuzzedDataProvider(data)
-        ext = supported_ext[fdp.ConsumeIntInRange(0, len(supported_ext)-1)]
         file_data = fdp.ConsumeBytes(atheris.ALL_REMAINING)
         temp_file = tempfile.NamedTemporaryFile()
         temp_file.write(file_data)
         temp_file.flush()
         fd = temp_file.name
-        # fd.name = "test" + ext
 
         try:
             imagesize.get(fd)
